0bf0e3a19fdc9f424f6deced21bd533f2cdb12f8
Charles R. Portwood II <charlesportwoodii@erianna.com>
2020-04-28 20:27:18 -0500

    working board definition

--- blver.py
+++ blver.py
@@ -10,6 +10,8 @@ def run(args):
         return toHex(args.toHex[0])
     elif (args.toSemVer != None):
         return toSemVer(args.toSemVer[0])
+    elif(args.toStringVer != None):
+        return toStringVer(args.toStringVer[0])
     else:
         return version()
 
@@ -31,10 +33,20 @@ def toSemVer(hex):
     split = [fullversion[i:i+3] for i in range(0, len(fullversion), 3)]
     return str(int(split[0])) + "." + str(int(split[1])) + "." + str(int(split[2]))
 
+def toStringVer(version):
+    """Converts a semantic version string to a string with leading zeros"""
+    split = version.split(".")
+    fullversion = ""
+    for i, v in enumerate(split, start=0):
+        fullversion += v.rjust(2, "0")
+
+
+    return fullversion
+
 parser = argparse.ArgumentParser(description='Converts a semantic version string to hex and back')
 parser.add_argument('--version', help='Specifies the version to use', action="store_true")
 parser.add_argument('--toHex', nargs="+", help='Converts a semantic version string to hex')
 parser.add_argument('--toSemVer', nargs="+", help='Converts a hex version string to a semantic version string')
-
+parser.add_argument('--toStringVer', nargs="+", help='Converts a semantic version string to one with leading zeros')
 args = parser.parse_args()
 print(run(args))
--- boards/pbrick/s140/include/custom_board.h
+++ boards/pbrick/s140/include/custom_board.h
@@ -37,9 +37,9 @@ extern "C" {
 // LEDs definitions for nRF52840-MDK
 #define LEDS_NUMBER    3
 
-#define LED_1          NRF_GPIO_PIN_MAP(0,5) // Blue
-#define LED_2          NRF_GPIO_PIN_MAP(0,4) // Green
-#define LED_3          NRF_GPIO_PIN_MAP(0,31) // Red
+#define LED_1          NRF_GPIO_PIN_MAP(0, 30) // Blue
+#define LED_2          NRF_GPIO_PIN_MAP(0, 31) // Green
+#define LED_3          NRF_GPIO_PIN_MAP(0, 04) // Red
 #define LED_START      LED_1
 #define LED_STOP       LED_3
 

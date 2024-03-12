1f11329ee3296bf0f8321b3e19abfb13ea1058bb
gb88 <root@vultr.guest>
2024-03-12 21:06:52 +0000

    old board

---
+++ boards/s140/ecg_nrf52_v2_3/include/config.h
@@ -0,0 +1,17 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#ifndef NRF_DFU_BLE_ADV_NAME
+#define NRF_DFU_BLE_ADV_NAME "ECG_DFU"
+#endif
+#define BLEDIS_MANUFACTURER "ddB0515"
+#define BLEDIS_MODEL "nRF52840 Board"
+  
+#define APP_USBD_VID 0x239A
+#define APP_USBD_PID 0x0029
+#endif
+
+#ifndef NRF_DFU_BL_ALLOW_DOWNGRADE
+#define NRF_DFU_BL_ALLOW_DOWNGRADE 1
+#endif
+
